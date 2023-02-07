e58f71e825184ff0e56b8da92b7a8dedd6269a28
Konrad Rudolph <konrad.rudolph@roche.com>
2023-02-07 15:29:56 +0100

    Version 0.0.1

---
+++ R/load.r
@@ -0,0 +1,31 @@
+load = function (renv = TRUE, env = TRUE) {
+    if (renv) load_renv()
+    if (env) load_project_env()
+    load_user_rprofile()
+}
+
+load_renv = function () {
+    if (file.exists("renv/activate.R")) {
+        sys.source("renv/activate.R", envir = .GlobalEnv)
+    }
+}
+
+load_user_rprofile = function () {
+    user_rprofile = Sys.getenv("R_PROFILE_USER", "~/.Rprofile")
+
+    if (file.exists(user_rprofile)) {
+        tryCatch({
+                sys.source(user_rprofile, envir = .GlobalEnv)
+                TRUE
+            },
+            error = function (e) {
+                warning(conditionMessage(e))
+                FALSE
+            }
+        )
+    }
+}
+
+load_project_env = function () {
+    if (file.exists(".env")) readRenviron(".env")
+}
