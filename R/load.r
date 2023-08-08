f070ec783b4dc4e7a922d2b160fccf69c80baa96
Konrad Rudolph <konrad.rudolph@roche.com>
2023-08-08 09:39:47 +0200

    Work around CRAN limitation on hooking `.First`

--- R/load.r
+++ R/load.r
@@ -27,7 +27,8 @@ load_user_rprofile = function () {
   user_rprofile = Sys.getenv("R_PROFILE_USER", "~/.Rprofile")
 
   if (file.exists(user_rprofile)) {
-    tryCatch({
+    tryCatch(
+      {
         sys.source(user_rprofile, envir = .GlobalEnv)
         TRUE
       },
@@ -53,13 +54,28 @@ load_dev_package = function () {
     return()
   }
 
-  first = get0(".First", envir = .GlobalEnv, mode = "function", ifnotfound = function () {})
+  # We want to load (but ideally not attach!) ‘devtools’, *if* it is installed.
+  # Simply adding it to the `defaultPackages` would cause a failure if it is not
+  # installed. And checking whether it is installed here may also fail, since
+  # this code is run before a potential ‘renv’ environment is set up, which may
+  # or may not have ‘devtools’.
+  # To work around this, we hook into the last package that will be loaded,
+  # and check *from there* whether ‘devtools’ is now available or not:
 
-  .GlobalEnv$.First = function () {
-    first()
-
-    if (requireNamespace("devtools", quietly = TRUE)) {
-      devtools::load_all(export_all = FALSE)
-    }
+  default_pkgs = getOption("defaultPackages")
+  if (length(default_pkgs) == 0L) {
+    # Something weird is going on, but okay.
+    return()
   }
+  last_pkg = default_pkgs[length(default_pkgs)]
+
+  setHook(
+    packageEvent(last_pkg, "onLoad"),
+    \(...) {
+      if (requireNamespace("devtools", quietly = TRUE)) {
+        devtools::load_all(export_all = FALSE)
+      }
+    },
+    "append"
+  )
 }
