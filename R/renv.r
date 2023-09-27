10fd422d06dc34600dcd6ea243a14fbbe95c4fc3
Konrad Rudolph <konrad.rudolph@roche.com>
2023-09-27 17:42:01 +0200

    Improve instructions for working with ‘renv’

--- R/renv.r
+++ R/renv.r
@@ -1,6 +1,6 @@
 load_renv = function () {
   if (file.exists('renv/activate.R')) {
-    # First, make ‘renv’ ignore ‘rprofile’ to avoid polluting the ‘renv’ status.
+    # Have ‘renv’ ignore ‘rprofile’ to avoid polluting the ‘renv’ status.
     renv_ignored_pkgs = getOption(
       'renv.settings.ignored.packages',
       default = getOption('renv.settings', list())$ignored.packages
